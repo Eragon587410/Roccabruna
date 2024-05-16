3ad8e5b0d1aa316291d939d016948034a3eceb49
Francesco Roccabruna <francesco.roccabruna@marconirovereto.it>
2024-05-16 15:07:48 +0200

    consegna

---
+++ src/MagazzinoSmartphone.java
@@ -0,0 +1,131 @@
+public class MagazzinoSmartphone {
+    private Smartphone[] smartphones;
+    private int count;
+
+    public MagazzinoSmartphone() {
+        smartphones = new Smartphone[10]; // Assuming initial capacity of 10
+        count = 0;
+    }
+
+    public MagazzinoSmartphone(MagazzinoSmartphone other) {
+        this();
+        if (other!=null){
+            for (int i = 0; i < other.getCount(); i++){
+                addSmartphone(other.smartphones[i]);
+            }
+        }
+    }
+
+
+    public void addSmartphone(Smartphone smartphone) {
+        if (smartphone != null){
+            smartphones[getCount()] = new Smartphone(smartphone);
+            count++;
+        }
+        
+    }
+
+    public void removeSmartphone(Smartphone smartphone) {
+        boolean trovato = false;
+        int posizione = 0;
+        int i = 0;
+        if (smartphone != null){
+            while (i < getCount() && !trovato){
+                if (this.smartphones[i].equals(smartphone)){
+                    trovato = true;
+                    posizione = i;
+                    
+                }
+                i++;
+            }
+            if (trovato){
+                for (int j = posizione; j < getCount()-1; j++){
+                    smartphones[j] = smartphones[j+1];
+                }
+                smartphones[getCount()-1] = null;
+                count--;
+            }
+        }
+    }
+
+    public Smartphone[] getSmartphonesArray() {
+        Smartphone[] smartphoneArray = new Smartphone[count];
+        System.arraycopy(smartphones, 0, smartphoneArray, 0, count);
+        return smartphoneArray;
+    }
+
+
+    public Smartphone getSmartphoneByBrand(String brand) {
+        Smartphone smartphone = null;
+        boolean trovato = false;
+        int i = 0;
+        while (i < getCount() && !trovato){
+            if (smartphones[i].getBrand().equals(brand)){
+                smartphone = new Smartphone(smartphones[i]);
+            }
+            i++;
+        }
+
+
+        return smartphone;
+    }
+
+    public MagazzinoSmartphone getSmartphonesByStorageCapacity(int storageCapacity) {
+        MagazzinoSmartphone magazzinoSmartphone = new MagazzinoSmartphone();
+        boolean trovato = false;
+
+        for (int i = 0; i < getCount(); i++){
+            if (smartphones[i].getStorageCapacity() == storageCapacity){
+                magazzinoSmartphone.addSmartphone(smartphones[i]);
+            }
+        }
+
+        return magazzinoSmartphone;
+    }
+
+    public MagazzinoSmartphone getSmartphonesByPriceRange(double minPrice, double maxPrice) {
+        MagazzinoSmartphone magazzinoSmartphone = new MagazzinoSmartphone();
+        boolean trovato = false;
+
+        for (int i = 0; i < getCount(); i++){
+            if (smartphones[i].getStorageCapacity() >= minPrice && smartphones[i].getStorageCapacity() <= maxPrice){
+                magazzinoSmartphone.addSmartphone(smartphones[i]);
+            }
+        }
+
+        return magazzinoSmartphone;
+    }
+
+    @Override
+    public String toString() {
+        String result = "\nMagazzinoSmartphone:\n";
+
+        for (int i = 0; i < getCount(); i++){
+            result += smartphones[i].toString()+"\n";
+        }
+        return result;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public boolean equals(Object obj) {
+        boolean output = false;
+        if (this == obj){
+            output = true;
+        } else {
+            if (obj instanceof MagazzinoSmartphone){
+                output = true;
+                for (int i = 0; i < ((MagazzinoSmartphone) obj).getCount(); i++){
+                    if (!((MagazzinoSmartphone) obj).smartphones[i].equals(smartphones[i])){
+                        output = false;
+                    }
+                }
+            }
+        }
+        return output;
+    }
+
+
+}
---
+++ src/Smartphone.java
@@ -0,0 +1,84 @@
+public class Smartphone {
+    private String brand;
+    private String model;
+    private int storageCapacity;
+    private double price;
+
+    public Smartphone(){
+        storageCapacity = 0;
+        price = 0;
+    };
+
+    public Smartphone(String brand, String model, int storageCapacity, double price) {
+        this();
+        setBrand(brand);
+        setModel(model);
+        setStorageCapacity(storageCapacity);
+        setPrice(price);
+    }
+
+    public Smartphone(Smartphone other) {
+        this(other.getBrand(), other.getModel(), other.getStorageCapacity(), other.getPrice());
+    }
+
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setStorageCapacity(int storageCapacity) {
+        this.storageCapacity = storageCapacity;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getStorageCapacity() {
+        return storageCapacity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Brand: " + getBrand() + ", Model: "+ getModel() + ", Storage Capacity: " + getStorageCapacity() + ", Price: " + getPrice();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        boolean output = false;
+        if (this == obj){
+            output = true;
+        } else {
+            if (obj instanceof Smartphone){
+                if (this != null){
+                    if (((Smartphone) obj).getBrand() != null && this.getBrand() !=null && ((Smartphone) obj).getModel()!=null && this.getModel()!=null && ((Smartphone) obj).getBrand().equals(this.getBrand()) && ((Smartphone) obj).getModel().equals(this.getModel()) && ((Smartphone) obj).getStorageCapacity() == this.getStorageCapacity() && ((Smartphone) obj).getPrice() == this.getPrice()){
+                        output = true;
+                    } else if (((Smartphone) obj).getStorageCapacity() == this.getStorageCapacity() && ((Smartphone) obj).getPrice() == this.getPrice() && this.getBrand() == ((Smartphone) obj).getBrand() && this.getModel() == ((Smartphone) obj).getModel()){
+                        output = true;
+                    }
+                }
+            }
+        }
+        
+        return output;
+    }
+
+    
+    
+}
